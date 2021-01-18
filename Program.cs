46b9fe3de1117e3578cb0bcd2546d82aec7bbb5a
Soton <77579218+soton371@users.noreply.github.com>
2021-01-18 19:57:26 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20, y = 30;
+            int i = add(x, y);
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+
+        static int add(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
